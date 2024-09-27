948196fdafb9e31933aa958370d71a6d554bd41e
VeeraKalakota <veerakalakota@gmail.com>
2024-09-26 21:06:38 -0700

    add

--- assets/js/rpg/Pickup.js
+++ assets/js/rpg/Pickup.js
@@ -7,10 +7,18 @@ export class Pickup {
         this.y = y; // Set initial y position
         this.image = new Image();
         this.image.src = imageSrc; // Load the starfish image
+        
+        // Ensure the image is drawn only after it's loaded
+        this.image.onload = () => {
+            this.loaded = true; // Flag to indicate the image is loaded
+        };
+        this.loaded = false; // Initially set to false
     }
 
     draw(ctx) {
-        ctx.drawImage(this.image, this.x, this.y);
+        if (this.loaded) { // Only draw if the image is loaded
+            ctx.drawImage(this.image, this.x, this.y);
+        }
     }
 }
 
